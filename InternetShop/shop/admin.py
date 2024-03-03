b4572fa07469f5517a232d22b65bc385ee150880
VasiliyHaroshka <horvasiliy@mail.ru>
2024-03-03 17:21:01 +0300

    Registration models to admin panel

--- InternetShop/shop/admin.py
+++ InternetShop/shop/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Registration of model 'Product' on admin panel"""
+    list_display = ("name", "slug", "price", "is_available", "created_at", "updated_at")
+    list_filter = ("is_available", "created_at", "updated_at")
+    list_editable = ("price", "is_available")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Registration of model 'Category' on admin panel"""
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
