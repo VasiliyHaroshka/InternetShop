333438feba49859da10cc895832292fee4244217
VasiliyHaroshka <horvasiliy@mail.ru>
2024-03-14 19:50:25 +0300

    add view for orders

--- InternetShop/orders/views.py
+++ InternetShop/orders/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from orders.forms import OrderForm
+from .models import OrderItem
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    count=item["count"],
+                )
+            context = {
+                "oreder": order,
+            }
+            cart.clear()
+            return render(request, "orders/order/created.html", context)
+    else:
+        form = OrderForm()
+    context = {
+        "cart": cart,
+        "form": form,
+    }
+    return render(request, "orders/order/create.html")
