f5214232dfdb4a08d94f99d6332e13995cd84723
VasiliyHaroshka <horvasiliy@mail.ru>
2024-03-17 15:19:45 +0300

    celery adjustment

--- InternetShop/InternetShop/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InternetShop.settings")
-
-app = Celery("InternetShop")
-app.config_from_object("django_conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
