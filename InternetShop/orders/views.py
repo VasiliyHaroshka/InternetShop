f9b7f6e23c65c8bd10dadb0130ce00d00ec3036b
VasiliyHaroshka <horvasiliy@mail.ru>
2024-03-25 18:11:08 +0300

    add payment redirect to orders

--- InternetShop/orders/views.py
+++ InternetShop/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from cart.cart import Cart
 from orders.forms import OrderForm
@@ -24,7 +25,8 @@ def order_create(request):
             }
             cart.clear()
             order_created.delay(order.id)
-            return render(request, "orders/order/created.html", context)
+            request.session["order_id"] = order.id
+            return redirect(reverse("payment:process"))
     else:
         form = OrderForm()
     context = {
