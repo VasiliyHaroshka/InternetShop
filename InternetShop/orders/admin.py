1810501661ea2b1efe142533a0cae99610c9dab5
VasiliyHaroshka <horvasiliy@mail.ru>
2024-04-14 12:22:56 +0300

    add stripe inf to admin panel

--- InternetShop/orders/admin.py
+++ InternetShop/orders/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import OrderItem, Order
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    raw_id_fields = ("product",) # field become Input
+    raw_id_fields = ("product",)  # field become Input
+
+
+def order_stripe_payment(obj: Order):
+    """Return url stripe payment"""
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        return mark_safe(f'<a href="{url}" target="_blank">{obj.stripe_id}</a>')
+    return ""
+
+
+order_stripe_payment.short_description = 'Stripe payment'
 
 
 @admin.register(Order)
