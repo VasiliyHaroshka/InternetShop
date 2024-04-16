041bbd571b558a78a7ee82e77288fb6f0da12922
VasiliyHaroshka <horvasiliy@mail.ru>
2024-04-16 19:46:30 +0300

    add action export to csv

--- InternetShop/orders/admin.py
+++ InternetShop/orders/admin.py
@@ -1,7 +1,34 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 
 from .models import OrderItem, Order
+
+
+def export_to_csv(request, modeladmin, queryset):
+    """Create action 'export to csv' for admin panel"""
+    opts = modeladmin.model._meta # текущий ModelAdmin
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])  # заголовки
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Export to CSV"
 
 
 class OrderItemInline(admin.TabularInline):
