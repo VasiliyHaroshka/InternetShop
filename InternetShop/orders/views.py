4c8c3e611dbb8281949737ea7d93e6bb828534c6
VasiliyHaroshka <horvasiliy@mail.ru>
2024-04-17 18:18:48 +0300

    add admin order detail info view

--- InternetShop/orders/views.py
+++ InternetShop/orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from cart.cart import Cart
 from orders.forms import OrderForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
 
 
@@ -32,3 +33,13 @@ def order_create(request):
         "form": form,
     }
     return render(request, "orders/order/create.html", context)
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    """Show the information about the order"""
+    order = get_object_or_404(Order, id=order_id)
+    context = {
+        "order": order,
+    }
+    return render(request, "admin/orders/order/detail.html", context)
