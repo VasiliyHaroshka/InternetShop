70a5cb8532c5ccd2f0dd67c534b70ebe8b9eb8a4
VasiliyHaroshka <horvasiliy@mail.ru>
2024-04-20 12:04:41 +0300

    add view for make report about orders in pdf

--- InternetShop/orders/views.py
+++ InternetShop/orders/views.py
@@ -1,6 +1,10 @@
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
 from django.urls import reverse
+import weasyprint
 
 from cart.cart import Cart
 from orders.forms import OrderForm
@@ -43,3 +47,23 @@ def admin_order_detail(request, order_id):
         "order": order,
     }
     return render(request, "admin/orders/order/detail.html", context)
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    """Generate report about the order in .pdf"""
+    order = get_object_or_404(Order, id=order_id)
+    context = {
+        "order": order,
+    }
+    html = render_to_string("orders/order/pdf.html", context)
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = f"filename=order_{order_id}.pdf"
+
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[
+            weasyprint.CSS(settings.STATIC_ROOT / "css/pdf.css")
+        ]
+    )
+    return response
