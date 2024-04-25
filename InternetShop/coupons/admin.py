7b630fa14557b7f6ffce029d6912d75373b88dc4
VasiliyHaroshka <horvasiliy@mail.ru>
2024-04-25 16:34:54 +0300

    register coupon model to admin panel

--- InternetShop/coupons/admin.py
+++ InternetShop/coupons/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Coupon
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = (
+        "code", "valid_from", "valid_to", "discount", "is_active"
+    )
+    list_filter = (
+        "valid_from", "valid_to", "is_active"
+    )
+    search_fields = (
+        "code",
+    )
