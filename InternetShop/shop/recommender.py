c2ec83ad32b5554f032a02c8a2ed5ade22b93fd7
VasiliyHaroshka <horvasiliy@mail.ru>
2024-05-09 12:57:03 +0300

    add method suggest_products to class recommender

--- InternetShop/shop/recommender.py
+++ InternetShop/shop/recommender.py
@@ -14,7 +14,8 @@ r = redis.Redis(
 class Recommender:
     """Class for work with recommendations of products"""
 
-    def get_product_key(self, id):
+    @staticmethod
+    def get_product_key(id):
         """Create key for Redis set"""
         return f"product:{id}:bought_with"
 
@@ -29,3 +30,39 @@ class Recommender:
                         1,
                         with_product_id,
                     )
+
+    def suggest_products(self, products: list[Product], max_count=6):
+        """Get products are bought together for recommendations"""
+        product_ids = [product.id for product in products]
+
+        if len(product_ids) == 1:
+            suggestions = r.zrange(
+                self.get_product_key(product_ids[0]),
+                0,
+                -1,
+                desc=True,
+            )[:max_count]
+
+        else:
+            str_ids = "".join([str(id) for id in product_ids])
+            tmp_key = f"tmp_{str_ids}"  # временный ключ
+
+            keys = [self.get_product_key(id) for id in product_ids]
+            r.zunionstore(tmp_key, keys)
+
+            r.zrem(tmp_key, *product_ids)  # del
+
+            suggestions = r.zrange(
+                tmp_key,
+                0,
+                -1,
+                desc=True,
+            )[:max_count]
+
+            r.delete(tmp_key)
+
+        suggestion_products_ids = [int(id) for id in suggestions]
+        suggestion_products = list[Product.objects.filter(id__in=suggestion_products_ids)]
+        suggestion_products.sort(key=lambda x: suggestion_products_ids.index(x.id))
+
+        return suggestion_products
