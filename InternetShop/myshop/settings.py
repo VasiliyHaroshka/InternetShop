a037e013a0e016d886cb5ead3fe95c49577d6945
VasiliyHaroshka <horvasiliy@mail.ru>
2024-05-13 09:44:20 +0300

    add localisation parameters to settings

--- InternetShop/myshop/settings.py
+++ InternetShop/myshop/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -123,7 +124,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en"
+
+LANGUAGES = [
+    ("en", "English"),
+    ("ru", "Русский"),
+]
 
 TIME_ZONE = "UTC"
 
