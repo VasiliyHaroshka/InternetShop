6ed14c0a7ac86e35522596375726eab73d725c3e
VasiliyHaroshka <horvasiliy@mail.ru>
2024-05-13 10:11:56 +0300

    add translation to cart and coupon forms

--- InternetShop/coupons/forms.py
+++ InternetShop/coupons/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 
 class CouponApplyForm(forms.Form):
     """Coupon form"""
-    code = forms.CharField(max_length=50)
+    code = forms.CharField(
+        max_length=50,
+        label=_("Code"),
+    )
