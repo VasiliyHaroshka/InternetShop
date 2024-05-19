7d993515dd250a17870d897c2a228da62c8d844e
VasiliyHaroshka <horvasiliy@mail.ru>
2024-05-19 11:16:50 +0300

    translate url to all apps

--- InternetShop/payment/urls.py
+++ InternetShop/payment/urls.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from django.urls import path
 
 from .views import payment_process, payment_completed, payment_canceled
@@ -6,8 +7,8 @@ from .webhooks import stripe_webhook
 app_name = "payment"
 
 urlpatterns = [
-    path("process/", payment_process, name="process"),
-    path("completed/", payment_completed, name="completed"),
-    path("canceled/", payment_canceled, name="canceled"),
+    path(_("process/"), payment_process, name="process"),
+    path(_("completed/"), payment_completed, name="completed"),
+    path(_("canceled/"), payment_canceled, name="canceled"),
     path("webhook/", stripe_webhook, name="stripe-webhook"),
 ]
