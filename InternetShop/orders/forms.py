a02f7bfaf5bd6682ff89021dfe583e7015a50d63
VasiliyHaroshka <horvasiliy@mail.ru>
2024-05-24 20:33:29 +0300

    add post_index validator to order form

--- InternetShop/orders/forms.py
+++ InternetShop/orders/forms.py
@@ -7,11 +7,11 @@ from .models import Order
 
 class OrderForm(forms.ModelForm):
     """Form of order creation"""
-    post_code = RUPostalCodeField()
+    post_index = RUPostalCodeField()
 
     class Meta:
         model = Order
         fields = (
             "first_name", "last_name", "email",
-            "city", "address", "post_code",
+            "city", "address", "post_index",
         )
