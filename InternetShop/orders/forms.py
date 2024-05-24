8563c6a54812488f7d90f223a68eed9b6e79d40d
VasiliyHaroshka <horvasiliy@mail.ru>
2024-05-24 20:27:59 +0300

    add post_code validator to order form

--- InternetShop/orders/forms.py
+++ InternetShop/orders/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 
+from localflavor.ru.forms import RUPostalCodeField
+
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
     """Form of order creation"""
+    post_code = RUPostalCodeField()
+
     class Meta:
         model = Order
         fields = (
             "first_name", "last_name", "email",
-            "city", "address", "post_index",
+            "city", "address", "post_code",
         )
