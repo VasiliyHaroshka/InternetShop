8bf24e9788a74a36a841c48dbd13209a60e9f1e8
VasiliyHaroshka <horvasiliy@mail.ru>
2024-06-06 16:13:12 +0300

    add mini photo to admin panel

--- InternetShop/shop/admin.py
+++ InternetShop/shop/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from parler.admin import TranslatableAdmin
 
@@ -8,7 +9,7 @@ from .models import Product, Category
 @admin.register(Product)
 class ProductAdmin(TranslatableAdmin):
     """Registration of model 'Product' on admin panel"""
-    list_display = ("name", "slug", "price", "category", "is_available", "created_at", "updated_at")
+    list_display = ("name", "get_photo", "price", "category", "is_available", "created_at", "updated_at")
     list_select_related = ("category",)
     list_display_links = ('name',)
     search_fields = ('name', 'category__name')
@@ -17,6 +18,11 @@ class ProductAdmin(TranslatableAdmin):
 
     def get_prepopulated_fields(self, request, obj=None):
         return {"slug": ("name",)}
+
+    @staticmethod
+    def get_photo(item):
+        if item.image:
+            return mark_safe(f"<img src='{item.image.url}' width=50>")
 
 
 @admin.register(Category)
